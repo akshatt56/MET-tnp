be3822a32f57d1f78c6b9efbac87e59cb912c394
akshatt56 <akshat.andhale@gmail.com>
2024-10-18 23:02:08 +0530

    Add files via upload
    Below Mentioned DSA Problems solutions:
    
    1) Find Largest Element in Array
    2) Find Second Largest Element in Array
    3) Check if Array is Sorted and Rotated
    4) Remove Duplicates from Sorted Array
    5) Find the Missing Number

---
+++ Solution.java
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int missingNumber(int[]nums)
+    {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        // Calculate the sum of elements in the array
+        for (int num : nums)
+        {
+            actualSum += num;
+        }
+
+        // The missing number = expected sum - actual sum
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = {3, 0, 1};
+        System.out.println("The missing number is: " + solution.missingNumber(nums));
+    }
+}
